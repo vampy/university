6d6f80dd1ce7b1e2d88006830219a362a015ebb3
Daniel Butum <danibutum@gmail.com>
2015-12-14 20:25:31 +0200

    Refactor OOP lab1

--- object-oriented-programming/labs/lab1/lab1.c
+++ object-oriented-programming/labs/lab1/lab1.c
@@ -14,11 +14,9 @@
 // return: 1 if true 0 otherwise
 int is_leap_year(int year)
 {
-    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         return 1;
-    }
-    
+
     return 0;
 }
 
@@ -30,20 +28,20 @@ int is_leap_year(int year)
 int read_data(int *year, int *day)
 {
     printf("The year: ");
-    if(scanf("%d", year) == 0)
+    if (scanf("%d", year) == 0)
     {
         printf("The year is not an number aborting");
         return 0;
     }
-    
+
     printf("The day: ");
-    if(scanf("%d", day) == 0)
+    if (scanf("%d", day) == 0)
     {
         printf("The day is not an number aborting");
         return 0;
     }
     printf("\n");
-    
+
     return 1;
 }
 
@@ -55,30 +53,28 @@ int read_data(int *year, int *day)
 int validate_data(int year, int day)
 {
     int limit_day = 365; // normal year
-    if(is_leap_year(year))
-    {
+    if (is_leap_year(year))
         limit_day++; // 366 leap year
-    }
-    
+
     // check year
-    if(year <= 0)
+    if (year <= 0)
     {
         printf("The year is not in AD >= 0\n");
         return 0;
     }
-    
+
     // check day
-    if(day <= 0)
+    if (day <= 0)
     {
         printf("The day must be strictly positive\n");
         return 0;
     }
-    if(day > limit_day)
+    if (day > limit_day)
     {
         printf("Not a valid day bigger than %d\n", limit_day);
         return 0;
     }
-    
+
     return 1;
 }
 
@@ -92,28 +88,24 @@ int validate_data(int year, int day)
 int find_month_and_day(int year, int day, int *found_month, int *found_day)
 {
     int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, i, temp_day = 0;
-    
+
     // make february longer
-    if(is_leap_year(year))
-    {
+    if (is_leap_year(year))
         months[1]++; // 29 days
-    }
-    
+
     // iterate and add the months
-    for(i = 0; i < 12; i++)
+    for (i = 0; i < 12; i++)
     {
         temp_day += months[i];
-        
-        if(DEBUG)
-        {
+
+        if (DEBUG)
             printf("DEBUG: i = %d, temp_day = %d, day = %d\n", i, temp_day, day);
-        }
-        
+
         // found the month i
-        if(temp_day >= day)
+        if (temp_day >= day)
         {
             *found_month = i + 1; // because of our indexing add +1
-            if(i > 0)
+            if (i > 0)
             {
                 // subtract from the temp_day which represents the
                 // days from the beginning of the year until the end
@@ -121,29 +113,29 @@ int find_month_and_day(int year, int day, int *found_month, int *found_day)
                 // then substract from that the number of days in this
                 // month
                 *found_day = abs(temp_day - day - months[i]);
-                
+
                 return 1;
             }
             // first month do nothing
             *found_day = day;
-            
+
             return 1;
         }
     }
-    
+
     // we should not reach here
     return 0;
 }
 
 int main()
 {
-    int read_year, read_day, i, found_month, found_day;
-    
+    int read_year, read_day, found_month, found_day;
+
     char *months_map[] = {
         "NULL", // not valid index 0
-        "January", 
-        "February", 
-        "March", 
+        "January",
+        "February",
+        "March",
         "April",
         "May",
         "June",
@@ -155,30 +147,24 @@ int main()
         "December"
     };
 
-    if(read_data(&read_year, &read_day) == 0)
-    {
+    if (read_data(&read_year, &read_day) == 0)
         exit(1);
-    }
-    
-    if(DEBUG)
-    {
+
+    if (DEBUG)
         printf("DEBUG: read from user --> year = %d, day = %d \n", read_year, read_day);
-    }
-    
-    if(validate_data(read_year, read_day) == 0)
-    {
+
+    if (validate_data(read_year, read_day) == 0)
         exit(1);
-    }
-    
-    if(find_month_and_day(read_year, read_day, &found_month, &found_day) == 0)
+
+    if (find_month_and_day(read_year, read_day, &found_month, &found_day) == 0)
     {
         printf("An internal error occured");
         exit(0);
     }
-    
+
     // print the result
-    printf("The full date(day/month/year): %02d/%02d/%04d \n", found_day, found_month, read_year);
-    printf("The full date with name(day/month/year): %02d/%s/%04d \n", found_day, months_map[found_month], read_year);
-    
+    printf("The full date (day/month/year): %02d/%02d/%04d \n", found_day, found_month, read_year);
+    printf("The full date with name (day/month/year): %02d/%s/%04d \n", found_day, months_map[found_month], read_year);
+
     return 0;
-} 
+}
--- object-oriented-programming/labs/lab1/lab1_teacher.c
+++ object-oriented-programming/labs/lab1/lab1_teacher.c
@@ -10,15 +10,13 @@
 // input:
 //       n - the number which we take the factorial
 // return:
-//       n! - the factorial 
+//       n! - the factorial
 int factorial(int n)
 {
     int i, n_fact = 1;
 
-    for(i = 1 ; i <= n ; i++) 
-    {
+    for (i = 1; i <= n; i++)
         n_fact = n_fact * i;
-    }
 
     return n_fact;
 }
@@ -29,27 +27,25 @@ int factorial(int n)
 void print_pascal_triangle(int dimension)
 {
     int k, i;
-    
-    for(k = 0 ; k < dimension; k++)
+
+    for (k = 0; k < dimension; k++)
     {
         // print the current row
         // Combination of k taken with i
-        for(i = 0 ; i <= k ; i++)
-        {
-            printf("%d ", factorial(k)/(factorial(i) * factorial(k - i)));
-        }
-        
+        for (i = 0; i <= k; i++)
+            printf("%d ", factorial(k) / (factorial(i) * factorial(k - i)));
+
         printf("\n");
     }
 }
- 
+
 int main()
 {
     int n;
-    
+
     printf("Dimension n = ");
     scanf("%d", &n);
-    
+
     print_pascal_triangle(n);
 
     return 0;
