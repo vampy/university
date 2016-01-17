8bc4ce9f3471ece8db770e6a56c58a75c472f365
Daniel Butum <danibutum@gmail.com>
2016-01-17 21:23:43 +0200

    Clean Statistics and probabilities lab 2

--- statistics-and-probabilities/labs/lab2/lab2.m
+++ statistics-and-probabilities/labs/lab2/lab2.m
@@ -1,6 +1,6 @@
 x = 0:0.01:3;
-f1 = (x.^2)./10;
-f2 = x.*sin(x);
+f1 = (x .^ 2) ./ 10;
+f2 = x .* sin(x);
 f3 = cos(x);
 % plot(x, f1, 'k:')
 % hold on
--- statistics-and-probabilities/labs/lab2/lab22.m
+++ statistics-and-probabilities/labs/lab2/lab22.m
@@ -2,11 +2,11 @@ n = input('n = ');
 a = input('a = ');
 b = input('b = ');
 i = 1:n; % index
-step = (b-a)/n;
+step = (b - a) / n;
 limits = a:step:b;
 left_lim = limits(i); % 1:n
-right_lim = limits(i+1); % 2:n
-mid_points = (right_lim + left_lim)/2;
+right_lim = limits(i + 1); % 2:n
+mid_points = (right_lim + left_lim) / 2;
 
 % matlabul merge pe coloane
 result = [i; left_lim; right_lim; mid_points];
