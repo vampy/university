4ea8f14dbda408a424cefb0c4d58547ef7ea4b15
Daniel Butum <danibutum@gmail.com>
2016-01-19 21:23:43 +0200

    Clean Statistics and probabilities lab 10

--- statistics-and-probabilities/labs/lab10/lab10.m
+++ statistics-and-probabilities/labs/lab10/lab10.m
@@ -35,32 +35,32 @@ x2_n = length(x2);
 x1_var = var(x1);
 x2_var = var(x2);
 
-pooled_var = ((x1_n - 1) * x1_var + (x2_n - 1) * x2_var)/(x1_n + x2_n - 2);
+pooled_var = ((x1_n - 1) * x1_var + (x2_n - 1) * x2_var) / (x1_n + x2_n - 2);
 pooled_std = sqrt(pooled_var);
 
-a_t1 = tinv(1 - alpha/2, x1_n + x2_n - 2);
+a_t1 = tinv(1 - alpha / 2, x1_n + x2_n - 2);
 
 % a)
-a_q_lower = x1_mean - x2_mean - a_t1 * pooled_std * sqrt(1/x1_n + 1/x2_n);
-a_q_upper = x1_mean - x2_mean + a_t1 * pooled_std * sqrt(1/x1_n + 1/x2_n);
+a_q_lower = x1_mean - x2_mean - a_t1 * pooled_std * sqrt(1 / x1_n + 1 / x2_n);
+a_q_upper = x1_mean - x2_mean + a_t1 * pooled_std * sqrt(1 / x1_n + 1 / x2_n);
 fprintf('a) The c.i (miu1 - miu2) where sigma1 = sigma2, is (%f, %f)\n\n', a_q_lower, a_q_upper);
 
 % b)
-c = (x1_var/x1_n) / (x1_var/x1_n + x2_var/x2_n);
-n = 1/(c^2/(x1_n - 1) + (1 - c)^2/(x2_n - 1));
+c = (x1_var / x1_n) / (x1_var /x1_n + x2_var / x2_n);
+n = 1 / (c ^ 2 / (x1_n - 1) + (1 - c) ^ 2 / (x2_n - 1));
 
 b_t1 = tinv(1 - alpha/2, n);
 
-b_q_lower = x1_mean - x2_mean - b_t1 * sqrt(x1_var/x1_n + x2_var/x2_n);
-b_q_upper = x1_mean - x2_mean + b_t1 * sqrt(x1_var/x1_n + x2_var/x2_n);
+b_q_lower = x1_mean - x2_mean - b_t1 * sqrt(x1_var / x1_n + x2_var / x2_n);
+b_q_upper = x1_mean - x2_mean + b_t1 * sqrt(x1_var / x1_n + x2_var / x2_n);
 fprintf('b) The c.i (miu1 - miu2) where sigma1 != sigma2, is (%f, %f)\n\n', b_q_lower, b_q_upper);
 
 % c)
-f1 = finv(1 - alpha/2, x1_n - 1, x2_n - 1);
-f2 = finv(alpha/2, x1_n - 1, x2_n - 1);
+f1 = finv(1 - alpha / 2, x1_n - 1, x2_n - 1);
+f2 = finv(alpha / 2, x1_n - 1, x2_n - 1);
 
-c_q_lower = 1/f1 * x1_var/x2_var;
-c_q_upper = 1/f2 * x1_var/x2_var;
+c_q_lower = 1 / f1 * x1_var / x2_var;
+c_q_upper = 1 / f2 * x1_var / x2_var;
 fprintf('c) The c.i for the ratio of the variances, is (%f, %f)\n\n', c_q_lower, c_q_upper);
 
 fprintf('The c.i for the ratio of the std deviations, is (%f, %f)\n\n', sqrt(c_q_lower), sqrt(c_q_upper));
